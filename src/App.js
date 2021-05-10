7f1b220f6f0ce035cc7a152e51f0240a277b9fb9
unvilio <dollarino1@mail.ru>
2021-05-10 22:37:13 +0600

    added localStorage

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ function App() {
   const [status, setStatus] = useState('all')
   const [filteredTodos, setFilteredTodos] = useState([])
 
+  useEffect(() => {
+    getLocalTodos()
+  }, [])
   useEffect (() => {
     filterHandler()
+    saveLocalTodos()
   }, [todos, status])
 
   const filterHandler = () => {
@@ -22,6 +26,17 @@ function App() {
       default:
         setFilteredTodos(todos)
         break;
+    }
+  }
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]))
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'))
+      setTodos(todoLocal)
     }
   }
   return (
