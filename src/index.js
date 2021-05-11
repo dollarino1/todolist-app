9ee5e06c2f61f05d1736aa3d689af915f3a41994
unvilio <dollarino1@mail.ru>
2021-05-11 16:26:44 +0600

    fixed some configs

--- src/index.js
+++ src/index.js
@@ -3,16 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './components/redux/reduxStore'
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
       <App />
-    </Provider>
-  </BrowserRouter>
 ,
   document.getElementById('root')
 );
